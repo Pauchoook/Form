8cef919c4a6d66ebccd6eb923ff637f0847bfb2d
Pauchoook <nkolatarasov@mail.ru>
2024-11-12 11:22:21 +0300

    add mask

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,11 @@
 import "../scss/style.scss";
 import modal from "./files/modal.js";
+import "./files/inputmask.js";
 
 modal();
+const inputs = document.querySelectorAll("input[type='tel']");
+
+if (inputs.length) {
+  let im = new Inputmask('+7 (999) 999-99-99');
+  im.mask(inputs);
+}
